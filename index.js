45d1641320efcc2cfdd39efe188affed8980bd08
Amber Hunt <amberhunt955@gmail.com>
2023-02-24 11:51:57 -0500

    created a startButton and playGame function - containing the game intro so far

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// Create the AlienSpaceship class
+//* Create the AlienSpaceship class
 class AlienSpaceship {
-    constructor(spaceshipName) {
-        this.spaceshipName = spaceshipName;
-        this.hull = Math.floor(Math.random()*4) + 3;
-        this.firepower = Math.floor(Math.random()*3) + 2;
-        this.accuracy = (Math.floor(Math.random()*3) + 6) / 10;
-    }
+  constructor(spaceshipName) {
+    this.spaceshipName = spaceshipName;
+    this.hull = Math.floor(Math.random() * 4) + 3;
+    this.firepower = Math.floor(Math.random() * 3) + 2;
+    this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
+  }
 
-    attack(spaceship) {
-
-    }
-
+  attack(spaceship) {}
 }
 
 // Create 6 alien spaceship objects
@@ -28,16 +25,52 @@ console.log(alienShip4);
 console.log(alienShip5);
 console.log(alienShip6);
 
-// Create the player spaceship object
+//* Create the player spaceship object
 const ussAssembly = {
-    spaceshipName: "USS Assembly",
-    hull: 20,
-    firepower: 5,
-    accuracy: 0.7,
-    
-    attack: function(spaceship) {
+  spaceshipName: "USS Assembly",
+  hull: 20,
+  firepower: 5,
+  accuracy: 0.7,
 
+  attack: function (spaceship) {
+    if (Math.random() < spaceship.accuracy) {
+      console.log("You have been hit!");
     }
-}
+  },
+};
 
 console.log(ussAssembly);
+
+//* Playing the game! 
+
+// Initialize a gameStart variable to false
+let gameStart = false;
+
+// Create a button to start the game when clicked
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", playGame);
+
+// Create the playGame function
+function playGame() {
+  // change gameStart to true
+  gameStart = true;
+
+  // Get the body element (to append and remove children later)
+  const body = document.querySelector("body");
+
+  // Create an intro to be displayed on the page
+  const intro = document.createElement("p");
+  intro.textContent =
+    "Earth has been attacked by a horde of aliens! You are the captain of the USS Assembly, on a mission to destroy every last alien ship. Battle the aliens as you try to destroy them with your lasers.";
+
+  // Create a next button to move forward in the game
+  const yourMove = document.createElement("button");
+  yourMove.classList.add("your-move");
+  yourMove.textContent = "Your Move!"
+
+  body.removeChild(startButton);
+  body.appendChild(intro);
+  body.appendChild(yourMove);
+}
+
+
