06bfaa66a019e49d6f98eb3e0fd97840f1e312cc
Amber Hunt <amberhunt955@gmail.com>
2023-02-24 10:30:20 -0500

    created alien class, 6 alien objects, and player object

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("Hello, world!");
+// Create the AlienSpaceship class
+class AlienSpaceship {
+    constructor(spaceshipName) {
+        this.spaceshipName = spaceshipName;
+        this.hull = Math.floor(Math.random()*4) + 3;
+        this.firepower = Math.floor(Math.random()*3) + 2;
+        this.accuracy = (Math.floor(Math.random()*3) + 6) / 10;
+    }
+
+    attack(spaceship) {
+
+    }
+
+}
+
+// Create 6 alien spaceship objects
+const alienShip1 = new AlienSpaceship("Alien Spaceship 1");
+const alienShip2 = new AlienSpaceship("Alien Spaceship 2");
+const alienShip3 = new AlienSpaceship("Alien Spaceship 3");
+const alienShip4 = new AlienSpaceship("Alien Spaceship 4");
+const alienShip5 = new AlienSpaceship("Alien Spaceship 5");
+const alienShip6 = new AlienSpaceship("Alien Spaceship 6");
+
+console.log(alienShip1);
+console.log(alienShip2);
+console.log(alienShip3);
+console.log(alienShip4);
+console.log(alienShip5);
+console.log(alienShip6);
+
+// Create the player spaceship object
+const ussAssembly = {
+    spaceshipName: "USS Assembly",
+    hull: 20,
+    firepower: 5,
+    accuracy: 0.7,
+    
+    attack: function(spaceship) {
+
+    }
+}
+
+console.log(ussAssembly);
