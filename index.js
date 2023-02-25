8315fbf3a83a95a94e4b3cb366b026b442ecdbb3
Amber Hunt <amberhunt955@gmail.com>
2023-02-24 21:28:05 -0500

    battle function working well in the console

--- index.js
+++ index.js
@@ -20,28 +20,35 @@ class Spaceship {
   attack(enemyShip) {
     // If the attacker accuracy is greater than a random generated number
     if (this.accuracy > Math.random()) {
-      // Determine damage to the enemy ship hull with a switch statement
+      // Determine damage to the enemy ship hull
       let damage = 0;
       switch (this.firepower) {
         case 2:
           damage = 1;
+          break;
         case 3:
           damage = 2;
+          break;
         case 4:
           damage = 3;
+          break;
         case 5:
           damage = 4;
+          break;
       }
 
       // Deduct damage from the enemy hull
-      console.log(`${this.name} strikes ${enemyShip.name} with a firepower of ${this.firepower}!`);
-      console.log(`Nice one! You have caused -${damage} points in damage.`);
+      console.log(
+        `%c${this.name} strikes ${enemyShip.name} with a firepower of ${this.firepower}!`,
+        `color: red;`
+      );
+      console.log(`-${damage} points in damage.`);
       enemyShip.hull -= damage;
       console.log(`${enemyShip.name} hull: ${enemyShip.hull}`);
 
       // If the attacker accuracy is less than a random generated number
     } else {
-      console.log("Damn! You missed.");
+      console.log(`${this.name} has missed!`);
     }
   }
 
@@ -92,7 +99,7 @@ startButton.addEventListener("click", playGame);
 // beginBattle.addEventListener("click", battleShip(alienShip1));
 // console.dir(beginBattle);
 
-// // Create a button for the user to attack 
+// // Create a button for the user to attack
 // const yourMove = document.createElement("button");
 // yourMove.textContent = "Your Move!";
 // yourMove.addEventListener("click", ussAssembly.attack(alienShip1));
@@ -107,26 +114,26 @@ startButton.addEventListener("click", playGame);
 function playGame() {
   gameInPlay = true;
   // gameScreen.removeChild(startButton);
-  
+
   // gameText.textContent =
   //   "Earth has been attacked by a horde of aliens! You are the captain of the USS Assembly, on a mission to destroy every last alien ship. Battle the aliens as you try to destroy them with your lasers.";
   // gameScreen.appendChild(gameText);
   // gameScreen.appendChild(beginBattle);
 
   battleShip(alienFleet[0]);
-
 }
 
 function battleShip(enemyShip) {
-    // gameScreen.removeChild(beginBattle);
-    // gameText = "It's your move! Shoot your lasers, quick!";
-    ussAssembly.attack(enemyShip);
+  // gameScreen.removeChild(beginBattle);
+  // gameText = "It's your move! Shoot your lasers, quick!";
+  ussAssembly.attack(enemyShip);
 
-    if (enemyShip.hull <= 0) {
-      console.log("You have defeated this spaceship!");
-      alienFleet.shift();
-      console.log(alienFleet);
-    } else {
-      console.log("Prepare for a counter attack!");
-    }
+  if (enemyShip.hull <= 0) {
+    console.log("You have defeated this spaceship!");
+    alienFleet.shift();
+    console.log(alienFleet);
+  } else {
+    console.log("Prepare for a counter attack!");
+    enemyShip.attack(ussAssembly);
+  }
 }
