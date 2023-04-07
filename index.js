c35543491fb24c13a1c3025ecf9941b07614f3f3
Amber Hunt <amberhunt955@gmail.com>
2023-04-06 22:29:02 -0400

    styling

--- index.js
+++ index.js
@@ -30,9 +30,11 @@ class Spaceship {
       enemyShip.hull -= damage;
       console.log(`-${damage} points in damage.`);
       console.log(`${enemyShip.name} hull: ${enemyShip.hull}`);
+      battleText.innerHTML += ` ${this.name} strikes ${enemyShip.name} with a firepower of ${this.firepower}, causing -${damage} points in damage!<br><br>${enemyShip.name} hull: ${enemyShip.hull}<br><br>${this.name} hull: ${this.hull}`
       // If the attacker accuracy is less than a random generated number
     } else {
       console.log(`${this.name} has missed!`);
+      battleText.innerHTML += `${this.name} has missed!`
     }
   }
 
@@ -42,7 +44,7 @@ class Spaceship {
         "%cYou have left the game. The game is over.",
         "color: green;"
       );
-      gameText.textContent = `You have left the game. The game is over.`
+      gameText.innerHTML = `You have left the game. The game is over.`
       gameScreen.appendChild(newGame);
       gameScreen.removeChild(yourMove);
     } 
@@ -64,7 +66,12 @@ startButton.addEventListener("click", function() {
 
 // Create a paragraph element to contain game text
 const gameText = document.createElement("p");
-gameText.textContent = "Earth has been attacked by a horde of aliens! You are the captain of the USS Assembly, on a mission to destroy every last alien ship. Battle the aliens as you try to destroy them with your lasers. \nThere are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.";
+gameText.innerHTML = `Earth has been attacked by a horde of aliens!<br><br>You are the captain of the USS Assembly, on a mission to destroy every last alien ship. Battle the aliens as you try to destroy them with your lasers.<br><br>There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time - they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order.<br><br>PLAYING THIS GAME: Press the "your move" button to initiate an attack! After you have destroyed a ship, you have the option to make a hasty retreat.<br><br>Best of luck!`;
+gameText.className = "text"
+
+// Create a paragraph to contain results
+const battleText = document.createElement("div");
+battleText.className = "text"
 
 // Create a your move button to trigger player move
 const yourMove = document.createElement("button");
@@ -105,12 +112,12 @@ function playGame() {
     battle(ussAssembly, alienFleet[0]);
   } else if (alienFleet.length === 0) {
     console.log(`%cThe game is over. You have won!`, `color: green;`);
-    gameText.textContent = `The game is over. You have won!`
+    gameText.innerHTML = `The game is over. You have won!`
     gameScreen.removeChild(yourMove);
     gameScreen.appendChild(newGame);
   } else {
     console.log(`%cThe game is over. You have lost.`, `color: green;`);
-    gameText.textContent = `The game is over. You have lost.`
+    gameText.innerHTML = `The game is over. You have lost.`
     gameScreen.removeChild(yourMove);
     gameScreen.appendChild(newGame);
   }
@@ -123,6 +130,10 @@ function battle(playerShip, enemyShip) {
     `%cBattling ${enemyShip.name} (hull: ${enemyShip.hull})`,
     `color:blue;`
   );
+
+  gameText.textContent = `Battling ${enemyShip.name}...`;
+  gameScreen.appendChild(battleText);
+  battleText.textContent = "";
   
   playerShip.attack(enemyShip);
   
@@ -130,15 +141,18 @@ function battle(playerShip, enemyShip) {
     alienFleet.shift();
     console.log("You have defeated this spaceship!");
     console.log(alienFleet);
+    battleText.innerHTML += "<br><br>You have defeated this spaceship!";
     let choice = confirm("Do you want to continue?"); // give option to retreat
     playerShip.retreatOrFight(choice);
   } else if (enemyShip.hull <= 0 && alienFleet.length === 1) {
     alienFleet.shift();
     console.log("You have defeated this spaceship!");
     console.log(alienFleet);
+    battleText.innerHTML += "<br><br>You have defeated this spaceship!"
     playGame(); // display game win message
   } else {
     console.log("Prepare for a counter attack!");
+    battleText.innerHTML += "<br><br>Time for a counter attack..."
     enemyShip.attack(playerShip);
   }
 
@@ -196,5 +210,8 @@ function gameOver() {
     alienShip6,
   ];
 
-  gameText.textContent = "Earth has been attacked by a horde of aliens! You are the captain of the USS Assembly, on a mission to destroy every last alien ship. Battle the aliens as you try to destroy them with your lasers. \nThere are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.";
+  gameText.innerHTML = `Earth has been attacked by a horde of aliens!<br><br>You are the captain of the USS Assembly, on a mission to destroy every last alien ship. Battle the aliens as you try to destroy them with your lasers.<br><br>There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time - they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order.<br><br>PLAYING THIS GAME: Press the "your move" button to initiate an attack! After you have destroyed a ship, you have the option to make a hasty retreat.<br><br>Best of luck!`;
+
+  battleText.textContent = "";
+  gameScreen.removeChild(battleText);
 }
